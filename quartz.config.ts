3fa02cbe67a7799d86f3f41357f30b004a0b6d29
Zion <zionbressanin@gmail.com>
2025-12-16 02:13:46 -0300

    Quartz sync: Dec 16, 2025, 2:13 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://zionbressanin-hub.github.io/Ferias-II/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
